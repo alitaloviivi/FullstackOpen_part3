eee024f9449c3abc2b1ecd74641d05b53bbb4c2d
alitaloviivi <viivi.alitalo@hotmail.com>
2025-10-09 13:44:55 +0300

    Exercises 3.7 -3.8

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json()) 
+app.use(morgan('tiny'))
+
+const morganFormat = morgan((tokens, req, res) => {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.method(req, res) === 'POST' ? JSON.stringify(req.body) : ''
+    ].join(' ')
+})
+  
+app.use(morganFormat)
 
 let persons = [
     { 
@@ -88,8 +103,14 @@ app.get('/info', (req, res) => {
     `;
   
     res.send(message);
-  });
+});
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+  
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
