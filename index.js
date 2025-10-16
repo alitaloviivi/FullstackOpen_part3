1a6731c92ec04646df3e93f014265bc2f62bcedc
alitaloviivi <viivi.alitalo@hotmail.com>
2025-10-16 12:12:47 +0300

    small fix

--- index.js
+++ index.js
@@ -83,8 +83,6 @@ app.post('/api/persons', (request, response, next) => {
 app.put('/api/persons/:id', (request, response, next) => {
     const { name, number } = request.body
 
-    console.log(person)
-
     Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson)
